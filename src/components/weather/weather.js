d58560c524457836876229062845ede539b396d7
38093 <nikolaef.post@gmail.com>
2021-06-09 20:28:44 +0300

    on preview

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -14,8 +14,7 @@ class Weather extends React.Component {
     }
 
     doRead() {
-
-        fetch("https://community-open-weather-map.p.rapidapi.com/forecast?q=Mykolayiv%2C%20Ua&units=metric&cnt=5", {
+        fetch("https://community-open-weather-map.p.rapidapi.com/forecast?q=Mykolayiv%2C%20Ua&units=metric&cnt=6", {
             "method": "GET",
             "headers": {
                 "x-rapidapi-key": "83eae3db6emsh556aa4a4d737577p16b150jsnd1ae8dcd55e0",
@@ -33,7 +32,6 @@ class Weather extends React.Component {
 
                     }
                 )
-                console.log(this.state.items);
             })
             .catch(err => {
                     this.setState(
@@ -60,47 +58,50 @@ class Weather extends React.Component {
     renderItems() {
         console.log(this.state.items)
         return (
-            <ul id='areaList'>
-
+            <>
                 <div className="main container">
                     <div className="row">
-                        <div className="col-xs-12">
-                            <div
-                                className="col-xs-12 col-sm-6 col-sm-offset-6 col-lg-6col-lg-offset-6 weather-panel">
-                                <div className="col-xs-6">
-                                    <h2>Mykolayiv, Ua<br/><small>{new Date().toLocaleDateString()}</small></h2>
-                                    <p className="h3"><img src={`http://openweathermap.org/img/wn/${this.state.items[0].weather[0].icon}@2x.png`} alt="car"/>{this.state.items[0].weather[0].main}
-                                    </p>
-                                </div>
-                                <div className="col-xs-6 text-center">
-                                    <div className="h1 temperature">
-                                        <span>12°</span>
-                                        <br/>
-                                        <small>8° / 13°</small>
+                        <div className="col-sm-12">
+                            <div className="col-xs-12 offset-sm-1 col-sm-10  col-lg-8 offset-lg-2  weather-panel">
+                                <div className="row align-items-center">
+                                    <div className="col-sm-6 pt-5">
+                                        <h2>Mykolayiv<br/><small>{new Date().toLocaleDateString()}</small></h2>
+                                        <div className="h3">
+                                            <img src={`http://openweathermap.org/img/wn/${this.state.items[0].weather[0].icon}@2x.png`}
+                                            alt="clouds"/>{this.state.items[0].weather[0].main}
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="col-xs-12">
-                                    <ul className="list-inline row forecast">
-                                        {
-                                            this.state.items.map(
-                                                item => (
-                                                    <li className="col-xs-4 col-sm-2 text-center">
-                                                        <h3 className="h5">Wed</h3>
-                                                        <p><i className="mi mi-fw mi-2x mi-cloud-sun"></i><br/>9°/18°
-                                                        </p>
-                                                    </li>
-
+                                    <div className="col-sm-6 text-center">
+                                        <div className="h1 temperature">
+                                            <span>{Math.floor(this.state.items[0].main.temp)}°</span>
+                                            <br/>
+                                            <small>{Math.floor(this.state.items[0].main.temp_min)}° / {Math.floor(this.state.items[0].main.temp_max)}°</small>
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-12 ">
+                                        <ul className="list-inline row forecast align-items-end m-1">
+                                            {
+                                                this.state.items.map(
+                                                    item => (
+                                                        <li className="col-xs-4 col-sm-4 col-lg-2 text-center pt-2">
+                                                            <h3 className="h5">{new Date(item.dt*1000).getHours()}:00</h3>
+                                                            <p><img src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+                                                                    alt="clouds"/>
+                                                                    <br/>
+                                                                {Math.floor(item.main.temp_min)}° / {Math.floor(item.main.temp_max)}°
+                                                            </p>
+                                                        </li>
+                                                    )
                                                 )
-                                            )
-                                        }
-                                    </ul>
+                                            }
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
-            </ul>
+            </>
         )
     }
 
